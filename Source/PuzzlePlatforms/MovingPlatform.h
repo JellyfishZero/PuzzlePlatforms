4159a06d5fb58749829c5fe7180a13acc0bcbaa1
海蜇零 <30751915+JellyfishZero@users.noreply.github.com>
2023-09-02 02:57:08 +0800

    Add MovingPlatform

---
+++ Source/PuzzlePlatforms/MovingPlatform.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "MovingPlatform.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORMS_API AMovingPlatform : public AStaticMeshActor
+{
+	GENERATED_BODY()
+	
+public:
+	AMovingPlatform();
+
+protected:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Basic Setting")
+	int32 Speed = 10;
+
+	virtual void Tick(float DeltaTime) override;
+};
