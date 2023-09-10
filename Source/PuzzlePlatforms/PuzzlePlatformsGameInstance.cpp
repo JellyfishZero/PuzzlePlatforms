45683e25c1e2b602a290b4be8762c65afcfadac0
海蜇零 <30751915+JellyfishZero@users.noreply.github.com>
2023-09-10 15:59:51 +0800

    Final

--- Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -2,4 +2,42 @@
 
 
 #include "PuzzlePlatformsGameInstance.h"
+#include "EngineGlobals.h"
+#include "Engine/Engine.h"
 
+UPuzzlePlatformsGameInstance::UPuzzlePlatformsGameInstance(const FObjectInitializer& ObjectInitializer)
+{
+	UE_LOG(LogTemp, Warning, TEXT("GameInstance Constructor"));
+}
+
+void UPuzzlePlatformsGameInstance::Init()
+{
+	Super::Init();
+	UE_LOG(LogTemp, Warning, TEXT("GameInstance Init"));
+}
+
+void UPuzzlePlatformsGameInstance::Host()
+{
+	UEngine* Engine = GetEngine();
+	if (!IsValid(Engine)) return;
+
+	Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, TEXT("Hosting"));
+
+	UWorld* World = GetWorld();
+	if (!IsValid(World)) return;
+	
+	World->ServerTravel("/Game/ThirdPerson/Maps/ThirdPersonMap?listen");
+}
+
+void UPuzzlePlatformsGameInstance::Join(const FString& Address)
+{
+	UEngine* Engine = GetEngine();
+	if (!IsValid(Engine)) return;
+
+	Engine->AddOnScreenDebugMessage(0, 2, FColor::Yellow, FString::Printf(TEXT("Join: %s"), *Address));
+
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+	if (!IsValid(PlayerController)) return;
+
+	PlayerController->ClientTravel(Address, ETravelType::TRAVEL_Absolute);
+}
--- Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.h
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.h
@@ -14,4 +14,15 @@ class PUZZLEPLATFORMS_API UPuzzlePlatformsGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 	
+public:
+
+	UPuzzlePlatformsGameInstance(const FObjectInitializer & ObjectInitializer);
+
+	virtual void Init() override;
+
+	UFUNCTION(Exec)
+	void Host();
+
+	UFUNCTION(Exec)
+	void Join(const FString& Address);
 };
